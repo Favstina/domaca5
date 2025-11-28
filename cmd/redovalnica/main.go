13b3cdfe30d9c9c9cdf639f6b0b78d2724378ccf
Favstina <fk68160@student.uni-lj.si>
2025-11-28 15:31:26 +0100

    its getting worse

--- cmd/redovalnica/main.go
+++ cmd/redovalnica/main.go
@@ -9,6 +9,20 @@ import (
 	"github.com/urfave/cli/v3"
 )
 
+func primerRedovalnice() map[string]redovalnica.Student {
+	studenti := make(map[string]redovalnica.Student)
+
+	prviStu := redovalnica.Student{"Anja", "Blasko", []int{2, 4, 8, 6, 9, 3, 7, 10}}
+	drugiStu := redovalnica.Student{"Rok", "Neki", []int{5, 7, 6, 8, 5}}
+	tretjiStu := redovalnica.Student{"Tomaz", "Priimek", []int{10, 9, 10, 8, 9, 8, 9, 10, 9}}
+
+	studenti["1"] = prviStu
+	studenti["2"] = drugiStu
+	studenti["3"] = tretjiStu
+
+	return studenti
+}
+
 func main() {
 	cmd := &cli.Command{
 		Name:  "Redovalnica",
@@ -38,7 +52,7 @@ func main() {
 				Name:  "izpis",
 				Usage: "Izpiši vse ocene",
 				Action: func(ctx context.Context, cmd *cli.Command) error {
-					studenti := loadTestData()
+					studenti := primerRedovalnice()
 					redovalnica.IzpisVsehOcen(studenti)
 					return nil
 				},
@@ -48,7 +62,7 @@ func main() {
 				Name:  "uspeh",
 				Usage: "Izpiši končni uspeh vseh študentov",
 				Action: func(ctx context.Context, cmd *cli.Command) error {
-					studenti := loadTestData()
+					studenti := primerRedovalnice()
 					redovalnica.IzpisiKoncniUspeh(studenti)
 					return nil
 				},
@@ -69,7 +83,7 @@ func main() {
 					min := cmd.Parent().Int("minOcena")
 					max := cmd.Parent().Int("maxOcena")
 
-					studenti := loadTestData()
+					studenti := primerRedovalnice()
 					redovalnica.DodajOceno(studenti, vpisna, ocena)
 
 					return nil
@@ -81,16 +95,4 @@ func main() {
 	if err := cmd.Run(context.Background(), os.Args); err != nil {
 		log.Fatal(err)
 	}
-}
-
-func primerRedovalnice() {
-	studenti := make(map[string]redovalnica.Student)
-
-	prviStu := Student{"Anja", "Blasko", []int{2, 4, 8, 6, 9, 3, 7, 10}}
-	drugiStu := Student{"Rok", "Neki", []int{5, 7, 6, 8, 5}}
-	tretjiStu := Student{"Tomaz", "Priimek", []int{10, 9, 10, 8, 9, 8, 9, 10, 9}}
-
-	studenti["1"] = prviStu
-	studenti["2"] = drugiStu
-	studenti["3"] = tretjiStu
 }
